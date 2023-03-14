bcb9c7dd90533bb3a76e6a010f42f4a168b7730c
Lyubava0382 <lubovkrivencova@gmail.com>
2023-03-14 16:48:18 +0300

    BackendVersion2.1

--- BackendNewsWebsite/src/main/java/ru/project/NewsWebsite/security/PersonDetails.java
+++ BackendNewsWebsite/src/main/java/ru/project/NewsWebsite/security/PersonDetails.java
@@ -1,6 +1,7 @@
 package ru.project.NewsWebsite.security;
 
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
---
+++ BackendNewsWebsite/src/test/java/ru/project/NewsWebsite/services/PostServiceTest.java
@@ -0,0 +1,52 @@
+package ru.project.NewsWebsite.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void like() {
+    }
+
+    @Test
+    void howMuchLikes() {
+    }
+
+    @Test
+    void deletePostById() {
+    }
+
+    @Test
+    void changePost() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void convertToPostDTO() {
+    }
+
+    @Test
+    void convertToPostDTOWithText() {
+    }
+
+    @Test
+    void convertToPost() {
+    }
+}
